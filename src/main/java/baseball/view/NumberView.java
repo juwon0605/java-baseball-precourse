18b4fb0fdb68be47cdaaf2dc403ff871e67cfa7b
juwon <58173061+juwon0605@users.noreply.github.com>
2021-12-15 21:31:18 +0900

    feat(NumberView.java): 숫자 입력 부분 구현

--- src/main/java/baseball/view/NumberView.java
+++ src/main/java/baseball/view/NumberView.java
@@ -1,10 +1,20 @@
 package baseball.view;
 
+import baseball.constant.Message;
 import baseball.model.Number;
+import camp.nextstep.edu.missionutils.Console;
 
 public class NumberView {
 
 	public Number getInput() {
-		return new Number();
+		System.out.println(Message.INPUT_MESSAGE_NUMBER);
+		try {
+			String input = Console.readLine();
+			System.out.println();
+			return new Number(input);
+		} catch (IllegalArgumentException e) {
+			System.out.println(Message.ERROR + e.getMessage() + "\n");
+			return getInput();
+		}
 	}
 }
